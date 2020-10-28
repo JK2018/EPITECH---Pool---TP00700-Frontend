ecf8194283a7837fcea32d0acdffe6f7f765e19d
Patrick <patrick.benzidane@epitech.eu>
2020-10-28 12:15:31 +0100

    Ajout page profile et mise en page

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import Login from './components/Login.vue'
 import Registration from './components/Registration.vue'
 import HomePage from "./components/HomePage";
+import Profil from "./components/Profil";
+
 
 export default [
     {path: '/', component: Login},
     {path: '/registration', component: Registration},
-    {path: '/dashboard', component: HomePage}
-
+    {path: '/dashboard', component: HomePage},
+    {path: '/profil', component: Profil},
 ]
