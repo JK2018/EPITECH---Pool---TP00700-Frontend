8f52d5e26d86ada79739c8a8d3ebf63fc2c9d4bf
Alex <commeau.aws@gmail.com>
2020-11-04 12:08:46 +0100

    add token/id/role/ in localstorage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="bdy">
     <div class="maindiv2">
-
       <div class="innerDiv left">
         <div class="description">
           <h1>Welcome to Time Manager</h1>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem Sed ut perspiciatis Sed ut perspiciatis unde omnis iste natus error sit voluptatem unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem Sed
+            ut perspiciatis Sed ut perspiciatis unde omnis iste natus error sit
+            voluptatem unde omnis iste natus error sit voluptatem accusantium
+            doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+            inventore veritatis et quasi architecto beatae vitae dicta sunt
+            explicabo.
+          </p>
           <!--<a href="#" class="square_btn">Create account</a>-->
           <nav class="routerbtn">
-            <router-link  to="/register">
+            <router-link to="/register">
               <span></span>
               <span></span>
               <span></span>
@@ -23,11 +29,11 @@
           <h1>Sign in</h1>
           <form>
             <div class="user-box">
-              <input v-model="email" type="email" name="" required>
+              <input v-model="email" type="email" name="" required />
               <label>Email Address</label>
             </div>
             <div class="user-box">
-              <input v-model="password" type="password" name="" required>
+              <input v-model="password" type="password" name="" required />
               <label>Password</label>
             </div>
             <nav class="routerbtn">
@@ -40,10 +46,8 @@
               </div>
             </nav>
           </form>
-
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -55,46 +59,47 @@
 import axios from "axios";
 
 export default {
-  name: 'login',
-  data(){
-    return{
+  name: "login",
+  data() {
+    return {
       password: "Michel",
       email: "Michel@michel.com",
-
-    }
+    };
   },
   methods: {
-    login(){
-      if(this.email.trim() !== "" && this.password.trim() !== ""){
-        axios.post("http://localhost:4000/sessions/sign_in", {
-          email: this.email,
-          password: this.password,
-        })
-            .then(function (res) {
-              console.log("Data = "+JSON.stringify(res,null,4));
-              if(res.status === 200){
-                localStorage.setItem("token",res.data.token);
-                // this.$router.push({name:'dashboard'});
-                window.location.href = "/app/dashboard"
-              }
-            })
-            .catch(function (err){
-              console.log("Err = "+err);
-            })
-      }else{
-        alert("Veuillez remplir vos champs")
+    login() {
+      if (this.email.trim() !== "" && this.password.trim() !== "") {
+        axios
+          .post("http://localhost:4000/sessions/sign_in", {
+            email: this.email,
+            password: this.password,
+          })
+          .then(function (res) {
+            console.log("Data = " + JSON.stringify(res, null, 4));
+            if (res.status === 200) {
+              localStorage.setItem("token", res.data.token);
+              localStorage.setItem("username", res.data.claims.username);
+              localStorage.setItem("role", res.data.claims.role);
+              localStorage.setItem("id", res.data.claims.id);
+              window.location.href = "/app/dashboard";
+            }
+          })
+          .catch(function (err) {
+            console.log("Err = " + err);
+          });
+      } else {
+        alert("Veuillez remplir vos champs");
       }
-    }
-  }
-
-}
+    },
+  },
+};
 </script>
 
 
 
 <style scoped>
-.bdy{
-  position:absolute;
+.bdy {
+  position: absolute;
   width: 100%;
   height: 100%;
   margin: 0;
@@ -103,7 +108,7 @@ export default {
   background: linear-gradient(to right, #cbcaa5, #334d50);
 }
 
-.maindiv2{
+.maindiv2 {
   display: flex;
   position: relative;
   top: 50%;
@@ -111,44 +116,37 @@ export default {
   margin-right: 10%;
   margin-left: 10%;
   height: 700px;
-  background-image: url('../assets/marble.jpg');
-
+  background-image: url("../assets/marble.jpg");
 }
-.innerDiv{
-  background-color:#272626;
-  box-shadow: 0 15px 25px rgba(0,0,0,.6);
+.innerDiv {
+  background-color: #272626;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
 }
-.innerDiv.left{
+.innerDiv.left {
   width: 60%;
 }
-.innerDiv.right{
+.innerDiv.right {
   width: 40%;
   opacity: 0.9;
 }
-.description{
+.description {
   position: relative;
   top: 50%;
   -ms-transform: translateY(-50%);
   transform: translateY(-50%);
   width: 60%;
-  margin:auto;
+  margin: auto;
   color: rgba(255, 255, 255, 0.555);
 }
-.loginForm{
+.loginForm {
   position: relative;
   top: 50%;
   -ms-transform: translateY(-50%);
   transform: translateY(-50%);
   width: 50%;
-  margin:auto;
+  margin: auto;
   color: rgba(255, 255, 255, 0.555);
 }
-
-
-
-
-
-
 
 .loginForm .user-box {
   position: relative;
@@ -167,13 +165,13 @@ export default {
 }
 .loginForm .user-box label {
   position: absolute;
-  top:0;
+  top: 0;
   left: 0;
   padding: 10px 0;
   font-size: 16px;
   color: rgba(255, 255, 255, 0.555);
   pointer-events: none;
-  transition: .5s;
+  transition: 0.5s;
 }
 
 .loginForm .user-box input:focus ~ label,
@@ -193,19 +191,17 @@ export default {
   text-decoration: none;
   text-transform: uppercase;
   overflow: hidden;
-  transition: .5s;
+  transition: 0.5s;
   margin-top: 40px;
-  letter-spacing: 4px
+  letter-spacing: 4px;
 }
 
 .routerbtn:hover {
-  background:  #ff8b5f;
+  background: #ff8b5f;
   color: rgba(255, 255, 255, 0.555);
   border-radius: 5px;
-  box-shadow: 0 0 5px  #ff8b5f,
-  0 0 25px #ff8b5f,
-  0 0 50px  #ff8b5f,
-  0 0 100px  #ff8b5f;
+  box-shadow: 0 0 5px #ff8b5f, 0 0 25px #ff8b5f, 0 0 50px #ff8b5f,
+    0 0 100px #ff8b5f;
 }
 
 .routerbtn span {
@@ -218,7 +214,7 @@ export default {
   left: -100%;
   width: 100%;
   height: 2px;
-  background: linear-gradient(90deg, transparent,  #ff8b5f);
+  background: linear-gradient(90deg, transparent, #ff8b5f);
   animation: btn-anim1 1s linear infinite;
 }
 
@@ -226,7 +222,8 @@ export default {
   0% {
     left: -100%;
   }
-  50%,100% {
+  50%,
+  100% {
     left: 100%;
   }
 }
@@ -236,16 +233,17 @@ export default {
   right: 0;
   width: 2px;
   height: 100%;
-  background: linear-gradient(180deg, transparent,  #ff8b5f);
+  background: linear-gradient(180deg, transparent, #ff8b5f);
   animation: btn-anim2 1s linear infinite;
-  animation-delay: .25s
+  animation-delay: 0.25s;
 }
 
 @keyframes btn-anim2 {
   0% {
     top: -100%;
   }
-  50%,100% {
+  50%,
+  100% {
     top: 100%;
   }
 }
@@ -255,16 +253,17 @@ export default {
   right: -100%;
   width: 100%;
   height: 2px;
-  background: linear-gradient(270deg, transparent,  #ff8b5f);
+  background: linear-gradient(270deg, transparent, #ff8b5f);
   animation: btn-anim3 1s linear infinite;
-  animation-delay: .5s
+  animation-delay: 0.5s;
 }
 
 @keyframes btn-anim3 {
   0% {
     right: -100%;
   }
-  50%,100% {
+  50%,
+  100% {
     right: 100%;
   }
 }
@@ -274,19 +273,18 @@ export default {
   left: 0;
   width: 2px;
   height: 100%;
-  background: linear-gradient(360deg, transparent,  #ff8b5f);
+  background: linear-gradient(360deg, transparent, #ff8b5f);
   animation: btn-anim4 1s linear infinite;
-  animation-delay: .75s
+  animation-delay: 0.75s;
 }
 
 @keyframes btn-anim4 {
   0% {
     bottom: -100%;
   }
-  50%,100% {
+  50%,
+  100% {
     bottom: 100%;
   }
 }
-
-
 </style>
